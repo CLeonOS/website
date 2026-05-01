02a5ef573bf1014cce9df712c411b93745ca3dda
JGZ_YES <luoriguodu@qq.com>
2026-05-01 21:45:44 +0800

    Add initial HTML structure for leonmmcoset.php

---
+++ dev/leonmmcoset.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>The CLeonOS offcial website</title>
+    </head>
+    <body>
+        <?php
+            include "../header.php";
+        ?>
+        <h1>LeonMMcoset</h1>
+        <hr>
+
+        <h2>-- Contact Information --</h2>
+        <p>Email : leonmmcoset@outlock.com</p>
+        <p>Github : LeonMMcoset</p>
+    </body>
+</html>
