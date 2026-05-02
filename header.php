ed486ec6e4bd698928c2ed328cf052b47352391a
Leonmmcoset <testmod@qq.com>
2026-05-02 15:32:56 +0800

    修改header

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <a href="/about.php">About</a> | 
     <a href="/contact.php">Contact</a> | 
     <a href="/blog.php">Blog</a> |
-    <b>Other Web | </b>
-    <a href="clospkg.ecuil.com">CLeonOS Package</a>
+    <a href="http://clospkg.ecuil.com">CLeonOS Package</a>
 </p>
 <hr>
