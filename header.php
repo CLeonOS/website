379c9c7e8d6b241e7b505de7c7996bdc231c2bcc
JGZ_YES <luoriguodu@qq.com>
2026-05-02 11:18:20 +0800

    Add link to CLeonOS Package in header

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
     <a href="/index.php">Home</a> | 
     <a href="/about.php">About</a> | 
     <a href="/contact.php">Contact</a> | 
-    <a href="/blog.php">Blog</a>
+    <a href="/blog.php">Blog</a> |
+    <b>Other Web | </b>
+    <a href="clospkg.ecuil.com">CLeonOS Package</a>
 </p>
 <hr>
